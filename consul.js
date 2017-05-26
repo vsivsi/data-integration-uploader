4556a8f7a74d47bba54746deba24ac82163b21dd
Chris Berthiaume <chrisbee@uw.edu>
2017-05-26 14:13:09 -0700

    Consul lookup then backup script pipeline.

---
+++ consul.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const stdin = process.stdin,
+    stdout = process.stdout;
+let inputJSON = '';
+
+stdin.setEncoding('utf8');
+
+stdin.on('data', function (chunk) {
+  inputJSON += chunk;
+});
+
+let parsedData;
+stdin.on('end', function () {
+  parsedData = JSON.parse(inputJSON);
+  // uncomment to simulate incompatible consul data
+  //parsedData.headers[0] = 'notreal';
+  console.error(parsedData.headers);
+  const outputJSON = JSON.stringify(parsedData, null, '  ');
+  stdout.write(outputJSON);
+  // Script should always exit with status 0 when key get/set
+  // is successful. Otherwise exit with error and client will
+  // see "Unknown Error"
+  process.exit(0);
+});
